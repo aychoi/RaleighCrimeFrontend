6f5b792ef299a89eed229ce7a02da89d095b4f20
Gokul Subramanian <guiltyspark@Gokuls-MacBook-Pro.local>
2015-02-15 12:14:09 -0500

    pulling from Nabil

--- static/js/animate.js
+++ static/js/animate.js
@@ -1,7 +1,7 @@
 // Now we've configured RequireJS, we can load our dependencies and start
 define([ 'ractive', 'rv!../ractive/template', "jquery", "velocity"], function ( Ractive, html, jquery, velocity) {
 
-	alert('test');
+	//alert('test');
 
     setInterval(function() {
       $("#searchContainer").velocity("callout.shake");
--- static/js/map.js
+++ static/js/map.js
@@ -7,16 +7,16 @@ define(
     function(mapbox)
 {
 
-var light_tiles = {
-  "tiles": [ "https://a.tiles.mapbox.com/v3/examples.map-20v6611k/{z}/{x}/{y}@2x.png" ],
-  "minzoom": 0,
-  "maxzoom": 18
-}
+	var light_tiles = {
+	  "tiles": [ "https://a.tiles.mapbox.com/v3/examples.map-20v6611k/{z}/{x}/{y}@2x.png" ],
+	  "minzoom": 0,
+	  "maxzoom": 18
+	}
 
-L.mapbox.accessToken = 'pk.eyJ1IjoiZ3VpbHR5c3BhcmsiLCJhIjoibEdBOVI3MCJ9.LpdbBjrh1L30CC5kiRQvIw';
-var map = L.mapbox.map('map', 'guiltyspark.kl48ij2n', {
-zoomControl: true })
-.setView([39.292222,-76.686667], 13);
+	L.mapbox.accessToken = 'pk.eyJ1IjoiZ3VpbHR5c3BhcmsiLCJhIjoibEdBOVI3MCJ9.LpdbBjrh1L30CC5kiRQvIw';
+	var map = L.mapbox.map('map', 'guiltyspark.kl48ij2n', {
+	zoomControl: true })
+	.setView([35.786090, -78.663556], 13);
 
-return map;
+	return map;
 });
